aaab22e2ee779bb84864072bbaa975c59fcd1586
DanielMarella <marella.daniel96@gmail.com>
2023-05-15 18:44:07 +0200

    add the event 'click' for visualize in console the final price of the ticket

--- js/myscript.js
+++ js/myscript.js
@@ -4,19 +4,36 @@ console.log(input_age.value);
 const userKm = document.getElementById(input_km);
 console.log(input_km.value);
 
-const FullTicket = (input_km.value * 0.233);
+let FullTicket = (input_km.value * 0.233);
 console.log(FullTicket)
+
+const DiscountTiket = FullTicket ;
+const button = document.querySelector('button');
 
 
 if (input_age.value < 18){
     const DiscountTiket = (19.4 / 100) * FullTicket;
-    console.log(DiscountTiket);
-    console.log(FullTicket - DiscountTiket);
+    console.log('il tuo sconto da minorenne è:' + DiscountTiket.toFixed(2) + '€');
+
+    const FinalPrice = (FullTicket - DiscountTiket);
+    button.addEventListener('click',
+    function(){
+        console.log('il tuo prezzo è:' + FinalPrice.toFixed(2)) ;
+    }
+);
 }   
 
-else if (input_age.value > 65){
+else if (input_age.value >= 65){
     const DiscountTiket = (37.7 / 100) * FullTicket;
-    console.log(DiscountTiket);
-    console.log(FullTicket - DiscountTiket);
+    console.log('il tuo sconto da over è:' + DiscountTiket.toFixed(2) + '€');
+
+    const FinalPrice = (FullTicket - DiscountTiket);
+    button.addEventListener('click',
+    function(){
+        console.log('il tuo prezzo è:' + FinalPrice.toFixed(2) + '€') ;
+    }
+);
 }
+
+
 
