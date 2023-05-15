4241cb591e9bde7bb3c6607abc8a9730228bb057
DanielMarella <marella.daniel96@gmail.com>
2023-05-15 17:19:26 +0200

    add the calculation of the full ticket, visualized in console

---
+++ js/myscript.js
@@ -0,0 +1,8 @@
+const userAge = document.getElementById(input_age);
+console.log(input_age.value);
+
+const userKm = document.getElementById(input_km);
+console.log(input_km.value);
+
+const FullTicket = (input_km.value * 0.233);
+console.log(FullTicket)
