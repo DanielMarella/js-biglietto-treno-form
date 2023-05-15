f01a1d8f4c0838bb05e30cb299dca9d05ba08c0b
DanielMarella <marella.daniel96@gmail.com>
2023-05-15 18:04:10 +0200

    add the price in console of -18 years and +=65 with discount

--- js/myscript.js
+++ js/myscript.js
@@ -6,3 +6,17 @@ console.log(input_km.value);
 
 const FullTicket = (input_km.value * 0.233);
 console.log(FullTicket)
+
+
+if (input_age.value < 18){
+    const DiscountTiket = (19.4 / 100) * FullTicket;
+    console.log(DiscountTiket);
+    console.log(FullTicket - DiscountTiket);
+}   
+
+else if (input_age.value > 65){
+    const DiscountTiket = (37.7 / 100) * FullTicket;
+    console.log(DiscountTiket);
+    console.log(FullTicket - DiscountTiket);
+}
+
